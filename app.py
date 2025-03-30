69fd1aa3da1d27d6da7e2e60b2490befae112012
IzabelaNajder <126763078+Izabela2001@users.noreply.github.com>
2025-03-30 21:04:10 +0200

    w
    bozw

---
+++ app.py
@@ -0,0 +1,16 @@
+from flasgger import Swagger
+from flask import Flask
+from config.database import engine, get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+import asyncio
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+@app.route('/')
+async def index():
+    async for db in get_db():
+        return "Połączenie z bazą danych działa!", 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
