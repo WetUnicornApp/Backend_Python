d8db7e7484cc4c64f358a780894da55da45b679e
hanczkamartanatalia <126960176+hanczkamartanatalia@users.noreply.github.com>
2025-04-18 20:54:20 +0200

    cross site

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flasgger import Swagger
 from flask import Flask
 from config.database import engine, get_db
@@ -7,6 +8,10 @@ from app.routes.user_routes import user_bp
 app = Flask(__name__)
 swagger = Swagger(app)
 app.register_blueprint(user_bp, url_prefix='/user')
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
 @app.route('/')
 async def index():
     async for db in get_db():
