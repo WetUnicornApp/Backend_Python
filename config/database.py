88960add7cc11677e240277584242b7cd6f65455
hanczkamartanatalia <126960176+hanczkamartanatalia@users.noreply.github.com>
2025-06-21 22:29:40 +0200

    more session with db

--- config/database.py
+++ config/database.py
@@ -3,13 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from dotenv import load_dotenv
+
 load_dotenv()  # wczyta plik .env
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 Base = declarative_base()
 assert DATABASE_URL, "Brak DATABASE_URL w środowisku"
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     db = SessionLocal()
